648e6f306205141d1b3394a13905cda2e2f3421d
Kaikai20040827 <1600268125@qq.com>
2025-12-06 18:21:17 +0800

    database setup

--- internal/pkg/database.go
+++ internal/pkg/database.go
@@ -1,21 +1,62 @@
 package pkg
 
 import (
-    "fmt"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
-    "github.com/Kaikai20040827/graduation/internal/config"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Kaikai20040827/graduation/internal/config"
+	"github.com/Kaikai20040827/graduation/internal/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-func NewDatabase(cfg *config.DatabaseConfig) (*gorm.DB, error) {
-    dsn := fmt.Sprintf(
-        "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        cfg.User,
-        cfg.Password,
-        cfg.Host,
-        cfg.Port,
-        cfg.Name,
-    )
+var DB *gorm.DB
 
-    return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func NewDatabase(cfg *config.DatabaseConfig) (*gorm.DB, error) {
+    //数据库登入基本信息
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name,
+	)
+
+    //配置访问时的日志模式
+    gormConfig := &gorm.Config{
+        Logger: logger.Default.LogMode(logger.Silent),
+    }
+
+    //创建一个数据库访问实例
+    db, err := gorm.Open(mysql.Open(dsn), gormConfig)
+    if err != nil {
+        return nil, err
+    }
+
+    //连接数据库
+    sqlDB, err := db.DB()
+    if err != nil {
+        return nil, err
+    }
+
+    //配置连接
+    sqlDB.SetConnMaxIdleTime(20) //最大闲置连接时间
+    sqlDB.SetConnMaxLifetime(time.Hour) //最大连接生命周期: Hour（小时）
+    sqlDB.SetMaxOpenConns(100) //最大打开的连接数量
+
+    //全局保存这个数据库访问
+    DB = db
+
+    if err := migrate(db); err != nil {
+		log.Printf("自动迁移失败: %v", err)
+	}
+
+    //无异常，则返回数据库
+    return db, nil
+}
+
+func migrate(db *gorm.DB) error {
+    return db.AutoMigrate(
+        &model.User{},
+        &model.File{},
+    )
 }
